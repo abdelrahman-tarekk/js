c9d6073776ff418ab9c6322a672ef67cd86eec19
abdelrahman-tarekk <abdoutarek889@gmail.com>
2025-04-15 13:49:35 +0200

    services js add

--- js/signup.js
+++ js/signup.js
@@ -6,43 +6,44 @@ var messageValidation = document.querySelector(".textToValidation");
 
 var users;
 
-if (localStorage.getItem("users") == null)
-  {
+if (localStorage.getItem("users") == null) {
   users = [];
-}
-else {
+} else {
   users = JSON.parse(localStorage.getItem("users"));
 }
 
-inputAddName.addEventListener("input", function(){
+inputAddName.addEventListener("input", function () {
   inputValidation(inputAddName);
-})
-inputAddEmail.addEventListener("input", function(){
+});
+inputAddEmail.addEventListener("input", function () {
   inputValidation(inputAddEmail);
-})
-inputAddPassword.addEventListener("input", function() {
+});
+inputAddPassword.addEventListener("input", function () {
   inputValidation(inputAddPassword);
-})
+});
 
-btnSignUp.addEventListener("click", function(){
-  if(inputValidation(inputAddName) && inputValidation(inputAddEmail) && inputValidation(inputAddPassword)){
+btnSignUp.addEventListener("click", function () {
+  if (
+    inputValidation(inputAddName) &&
+    inputValidation(inputAddEmail) &&
+    inputValidation(inputAddPassword)
+  ) {
     signUp();
     messageValidation.classList.remove("d-none");
     messageValidation.classList.add("d-block");
     messageValidation.style.color = "#28a745";
     messageValidation.innerHTML = "Success";
 
-    window.location.href = "login.html"; 
-  }
-  else{
+    window.location.href = "login.html";
+  } else {
     messageValidation.classList.remove("d-none");
     messageValidation.classList.add("d-block");
     messageValidation.style.color = "red";
     messageValidation.innerHTML = "Please input the valid data";
   }
-})
-function signUp(){
+});
 
+function signUp() {
   var name = inputAddName.value;
   var email = inputAddEmail.value;
   var password = inputAddPassword.value;
@@ -50,50 +51,63 @@ function signUp(){
   var user = {
     name: name,
     email: email,
-    password: password
-  }
+    password: password,
+  };
+
   users.push(user);
   localStorage.setItem("users", JSON.stringify(users));
 
   inputAddName.value = null;
   inputAddEmail.value = null;
   inputAddPassword.value = null;
-  inputAddName.classList.remove("is-valid");
-  inputAddName.classList.remove("is-invalid");
-  inputAddEmail.classList.remove("is-valid");
-  inputAddEmail.classList.remove("is-invalid");
-  inputAddPassword.classList.remove("is-valid");
-  inputAddPassword.classList.remove("is-invalid");
+  inputAddName.classList.remove("is-valid", "is-invalid");
+  inputAddEmail.classList.remove("is-valid", "is-invalid");
+  inputAddPassword.classList.remove("is-valid", "is-invalid");
 }
 
 function inputValidation(element) {
-
   var text = element.value;
   var regex = {
     registerName: /^\w{3,30}$/,
     registerEmail: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
     registerPassword: /^[0-9a-zA-Z]{6,}$/,
-  } 
+  };
 
-    if (element.id === "registerEmail") {
-    var isDuplicate = users.some(function(user) {
-      return user.email.toLowerCase() === text.toLowerCase()});
-    if (isDuplicate) {
+  // Check for duplicate email
+  if (element.id === "registerEmail") {
+    var isDuplicateEmail = users.some(function (user) {
+      return user.email.toLowerCase() === text.toLowerCase();
+    });
+
+    if (isDuplicateEmail) {
       element.classList.add("is-invalid");
       element.classList.remove("is-valid");
       messageValidation.classList.remove("d-none");
       messageValidation.classList.add("d-block");
       messageValidation.style.color = "red";
       messageValidation.innerHTML = "This email already exists";
-    }
-    else{
-      element.classList.add("is-valid");
-      element.classList.remove("is-invalid");
-      messageValidation.classList.add("d-none");
+      return false;
     }
   }
 
+  // Check for duplicate name
+  if (element.id === "registerName") {
+    var isDuplicateName = users.some(function (user) {
+      return user.name.toLowerCase() === text.toLowerCase();
+    });
 
+    if (isDuplicateName) {
+      element.classList.add("is-invalid");
+      element.classList.remove("is-valid");
+      messageValidation.classList.remove("d-none");
+      messageValidation.classList.add("d-block");
+      messageValidation.style.color = "red";
+      messageValidation.innerHTML = "This username already exists";
+      return false;
+    }
+  }
+
+  // Password validation
   if (element.id === "registerPassword" && !regex.registerPassword.test(text)) {
     element.classList.add("is-invalid");
     element.classList.remove("is-valid");
@@ -101,21 +115,17 @@ function inputValidation(element) {
     messageValidation.classList.add("d-block");
     messageValidation.style.color = "red";
     messageValidation.innerHTML = "Password must contain at least 6 characters";
+    return false;
   }
-  else{
+
+  if (regex[element.id].test(text)) {
     element.classList.add("is-valid");
     element.classList.remove("is-invalid");
     messageValidation.classList.add("d-none");
-  }
-
-
-  if(regex[element.id].test(text)){
-    element.classList.add("is-valid");
-    element.classList.remove("is-invalid");
     return true;
   } else {
     element.classList.add("is-invalid");
     element.classList.remove("is-valid");
     return false;
   }
-  }
+}
